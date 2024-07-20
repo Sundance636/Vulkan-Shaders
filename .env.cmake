f5962e6a8a015bef96c1f110634fc7c152a12f9c
Sundance636 <s.clement636@gmail.com>
2024-07-20 06:28:30 -0600

    edit .env

--- .env.cmake
+++ .env.cmake
@@ -1,8 +1,9 @@
+# Set the path libraries
 set(GLFW_PATH C:/libs/glfw-3.4.bin.WIN64)
 set(GLM_PATH C:/libs/glm)
 set(VULKAN_SDK_PATH C:/VulkanSDK/1.3.283.0)
  
-# Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX
+# Set MINGW_PATH
 set(MINGW_PATH C:/libs/winlibs-x86_64-posix-seh-gcc-14.1.0-llvm-18.1.8-mingw-w64ucrt-12.0.0-r3/mingw64)
  
 # Optional set TINYOBJ_PATH to target specific version, otherwise defaults to external/tinyobjloader
